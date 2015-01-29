633c4ddeaa3a4b13905da9c3eaf43f5d482bab2a
toeloese <sophie.moons@student.kdg.be>
2015-01-29 10:32:57 +0100

    Author namen verandert naar epische levels B)

---
+++ Digital_Broadcast_Trivia_Game_Java_Project/src/trivia_game_xlet/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package trivia_game_xlet;
+
+/**
+ *
+ * @author Nicolas THE Cock, Sophie Moons (and beyond)
+ */
+public class Main {
+
+}
